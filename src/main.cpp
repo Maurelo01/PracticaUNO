09b8bdc2c753a4b19daea11d32078a7a10497741
maurelo860@gmail.com <maurelo860@gmail.com>
2026-02-17 12:54:31 -0600

    Se añadio el flujo de juego, pendiente de correcciones, funcional

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
-#include <string>
+#include <limits>
+#include "../include/Juego.h"
+
 using namespace std;
 
-int main()
+void limpiarBufferMain()
 {
-    string name = "World";
-    cout << "Hello " << name;
+    string sueltos; 
+    getline(cin, sueltos);
+}
+
+int main() 
+{
+    Juego miJuego;
+    ReglasFlags reglas;
+    int numJugadores;
+    char opc;
+    cout << " ______________________________________" << endl;
+    cout << "|___________UNO CLASICO/FLIP___________|" << endl;
+    do 
+    {
+        cout << "Ingresa la cantidad de jugadores (Minimo 2): ";
+        cin >> numJugadores;
+        if(cin.fail())
+        {
+            cin.clear(); limpiarBufferMain();
+        }
+    }
+    while (numJugadores < 2);
+    cout << "Reglas FLAG: " << endl;
+    cout << "¿Activar UNO FLIP? (s/n): ";
+    cin >> opc;
+    reglas.modoFlip = (opc == 's' || opc == 'S');
+
+    if (!reglas.modoFlip)
+    {
+        cout << "¿Permitir acumulacion +2 sobre +2? (s/n): ";
+        cin >> opc;
+        reglas.acumulacion = (opc == 's' || opc == 'S');
+    }
+    miJuego.inicializar(numJugadores, reglas);
+    miJuego.flujoPrincipal();
     return 0;
 }
