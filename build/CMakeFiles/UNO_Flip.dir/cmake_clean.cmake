260710c2b44cb25d328792e905c47c4e9d564f8c
maurelo860@gmail.com <maurelo860@gmail.com>
2026-02-22 14:12:07 -0600

    Se creo que el archivo Makefile por CMakeList.txt yy se corrigio el problema de cuando la partida inicia en comodin

---
+++ build/CMakeFiles/UNO_Flip.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UNO_Flip.dir/link.d"
+  "CMakeFiles/UNO_Flip.dir/src/Carta.cpp.o"
+  "CMakeFiles/UNO_Flip.dir/src/Carta.cpp.o.d"
+  "CMakeFiles/UNO_Flip.dir/src/Juego.cpp.o"
+  "CMakeFiles/UNO_Flip.dir/src/Juego.cpp.o.d"
+  "CMakeFiles/UNO_Flip.dir/src/Jugador.cpp.o"
+  "CMakeFiles/UNO_Flip.dir/src/Jugador.cpp.o.d"
+  "CMakeFiles/UNO_Flip.dir/src/main.cpp.o"
+  "CMakeFiles/UNO_Flip.dir/src/main.cpp.o.d"
+  "UNO_Flip"
+  "UNO_Flip.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UNO_Flip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -10,54 +10,62 @@ void limpiarBufferMain()
     getline(cin, sueltos);
 }
 
+void limpiarPantalla()
+{
+    cout << "\033[2J"; // ANSI para limpiar pantalla
+}
+
 int main() 
 {
     Juego miJuego;
     ReglasFlags reglas;
     int numJugadores;
     char opc;
-    cout << " ______________________________________" << endl;
-    cout << "|___________UNO CLASICO/FLIP___________|" << endl;
     do 
     {
+        cout << " ______________________________________" << endl;
+        cout << "|___________UNO CLASICO/FLIP___________|" << endl;
         cout << "Ingresa la cantidad de jugadores (Minimo 2): ";
         cin >> numJugadores;
+        limpiarPantalla();
         if(numJugadores < 2)
         {
-            cout << "El minimo de jugadores es 2. Intenta de nuevo." << endl;
+            cout << "El minimo de jugadores es 2, intenta de nuevo." << endl;
         }
         if(cin.fail())
         {
-            cin.clear(); limpiarBufferMain();
+            cin.clear();
+            limpiarBufferMain();
         }
     }
     while (numJugadores < 2);
-    cout << " REGLAS FLAG " << endl;
-    cout << "1. ¿Activar UNO FLIP? (s/n): ";
+    cout << " ______________________________________" << endl;
+    cout << "|_____________REGLAS FLAG_____________|" << endl;
+    cout << "-> ¿Activar UNO FLIP? (s/n): ";
     cin >> opc; 
     reglas.modoFlip = (opc == 's' || opc == 'S');
     if (opc == 's' || opc == 'S')
     {
-        cout << "2. ¿Permitir acumulacion? (Lado Claro: +1 sobre +1/+2, +2 sobre +2 | Lado Oscuro (Flip): +3 sobre +3/+6, +6 sobre +6)(s/n): ";
+        cout << "-> ¿Permitir acumulacion? (Lado Claro: +1 sobre +1/+2, +2 sobre +2 | Lado Oscuro (Flip): +3 sobre +3/+6, +6 sobre +6)(s/n): ";
         cin >> opc;
         reglas.acumulacion = (opc == 's' || opc == 'S');
     }
     else if (opc == 'n' || opc == 'N')
     {
-        cout << "2. ¿Permitir acumulacion? (+2 sobre +2/+4, +4 sobre +4)(s/n): ";
+        cout << "-> ¿Permitir acumulacion? (+2 sobre +2/+4, +4 sobre +4)(s/n): ";
         cin >> opc;
         reglas.acumulacion = (opc == 's' || opc == 'S');
-        cout << "3. ¿Activar el Reto del +4? (s/n): ";
+        cout << "-> ¿Activar el Reto del +4? (s/n): ";
         cin >> opc;
         reglas.retoMasCuatro = (opc == 's' || opc == 'S');
     }
-    cout << "4. ¿Robar cartas hasta poder jugar? (s/n): ";
+    cout << "-> ¿Robar cartas hasta poder jugar? (s/n): ";
     cin >> opc;
     reglas.roboHastaJugar = (opc == 's' || opc == 'S');
-    cout << "5. ¿Castigo de tomar 2 cartas por no gritar UNO? (s/n): ";
+    cout << "-> ¿Castigo de tomar 2 cartas por no gritar UNO? (s/n): ";
     cin >> opc;
     reglas.gritoUno = (opc == 's' || opc == 'S');
-    cout << "6. ¿Permitir ganar lanzando carta negra(Comodín) al final? (s/n): ";
+    cout << "-> ¿Permitir ganar lanzando carta negra(Comodín) al final? (s/n): ";
     cin >> opc;
     reglas.ganarConNegra = (opc == 's' || opc == 'S');
     limpiarBufferMain();
