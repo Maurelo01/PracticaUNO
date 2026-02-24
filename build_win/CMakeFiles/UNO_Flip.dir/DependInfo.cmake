7f15da4bd1e2446b19669ea0fb67af2edd0be278
maurelo860@gmail.com <maurelo860@gmail.com>
2026-02-24 13:55:20 -0600

    Se creo el makefile para generar el .exe

---
+++ build_win/CMakeFiles/UNO_Flip.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mauricio/Escritorio/Tarea/Estructura de Datos/Practica 1/src/Carta.cpp" "CMakeFiles/UNO_Flip.dir/src/Carta.cpp.obj" "gcc" "CMakeFiles/UNO_Flip.dir/src/Carta.cpp.obj.d"
+  "/home/mauricio/Escritorio/Tarea/Estructura de Datos/Practica 1/src/Juego.cpp" "CMakeFiles/UNO_Flip.dir/src/Juego.cpp.obj" "gcc" "CMakeFiles/UNO_Flip.dir/src/Juego.cpp.obj.d"
+  "/home/mauricio/Escritorio/Tarea/Estructura de Datos/Practica 1/src/Jugador.cpp" "CMakeFiles/UNO_Flip.dir/src/Jugador.cpp.obj" "gcc" "CMakeFiles/UNO_Flip.dir/src/Jugador.cpp.obj.d"
+  "/home/mauricio/Escritorio/Tarea/Estructura de Datos/Practica 1/src/main.cpp" "CMakeFiles/UNO_Flip.dir/src/main.cpp.obj" "gcc" "CMakeFiles/UNO_Flip.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
